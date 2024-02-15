1d20005e177cd77249ac3a342aa4ad78803041cf
Rafał <rafalgondek@onet.pl>
2024-02-15 21:08:36 +0100

    added adding users to database with prisma and mongodb

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <input type="text" placeholder="email" />
+      <input type="text" placeholder="password" />
+      <button>LOGIN</button>
+    </div>
+  );
+};
+
+export default LoginPage;
