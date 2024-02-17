d08902523b719c38ccced5f6c29788f8cda5c226
Rafał <rafalgondek@onet.pl>
2024-02-17 19:13:41 +0100

    changes in authjs, protecting routes is working but i have no idea how to signin

--- actions/login.ts
+++ actions/login.ts
@@ -1,1 +1,2 @@
 "use server";
+import { db } from "@/lib/db";
--- actions/register.ts
+++ actions/register.ts
@@ -1,16 +1,13 @@
 "use server";
 import bcrypt from "bcrypt";
 import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (data: any) => {
   const { name, email, pass } = data;
   const hashedPass = await bcrypt.hash(pass, 10);
 
-  const exisitingUser = await db.user.findUnique({
-    where: {
-      email,
-    },
-  });
+  const exisitingUser = await getUserByEmail(email);
 
   if (exisitingUser) {
     console.log("email in use");
