0c3461394c3a56e52093bcbc05bb6a06b8018bab
Tuptuus <rafalgondek0@gmail.com>
2024-05-23 20:41:13 +0200

    small changes, surveys appear real time, preparation for questions page for survey

---
+++ app/components/SideBar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function SideBar() {
+  return (
+    <div className="">
+      <div className="cursor-pointer py-3 rounded-xl pl-3 w-52 bg-orange-500">
+        Pytania
+      </div>
+      <div className="cursor-pointer py-3 rounded-xl pl-3 w-52 mt-3">
+        Odpowiedzi
+      </div>
+    </div>
+  );
+}
+
+export default SideBar;
