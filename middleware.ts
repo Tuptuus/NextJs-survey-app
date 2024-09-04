c2f7492b2f8fc30a977f07154a4512a0789f6e2c
Rafał Gondek <rafalgondek0@gmail.com>
2024-09-04 21:02:41 +0200

    stopped tracking .env

--- middleware.ts
+++ middleware.ts
@@ -13,17 +13,17 @@ export default auth((req) => {
   const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
 
-  // const trustedHosts = [
-  //   "localhost:3000",
-  //   process.env.NEXTAUTH_URL?.replace(/^https?:\/\//, ""),
-  // ];
-  // console.log("host: ", nextUrl.host);
-  // console.log("trusted: ", trustedHosts);
+  const trustedHosts = [
+    "localhost:3000",
+    process.env.NEXTAUTH_URL?.replace(/^https?:\/\//, ""),
+  ];
+  console.log("host: ", nextUrl.host);
+  console.log("trusted: ", trustedHosts);
 
-  // if (!trustedHosts.includes(nextUrl.host)) {
-  //   console.error(`Untrusted host: ${nextUrl.host}`);
-  //   return new Response("Untrusted Host", { status: 403 });
-  // }
+  if (!trustedHosts.includes(nextUrl.host)) {
+    console.error(`Untrusted host: ${nextUrl.host}`);
+    return new Response("Untrusted Host", { status: 403 });
+  }
 
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
